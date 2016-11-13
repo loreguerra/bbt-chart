179df14a228c34ded484b122e3da1287554f95f9
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-11-13 11:58:42 -0800

    imported plotly, set up temps data

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
 from set_credentials import set_credentials
 
 set_credentials()
+
+temps = [(10,97.36), (11,97.43), (12,97.37), (13,98.18)]
