8d3992c7381ce0984a24616c25af2b31997304be
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-11-15 09:40:10 -0800

    notes added

--- run.py
+++ run.py
@@ -7,6 +7,9 @@ set_credentials()
 days = [10, 11, 12, 13, 14]
 temps = [97.36, 97.43, 97.37, 98.18, 96.9]
 
+#install twitter tools, get data, clean up data, organize data
+#gather data from @mentions on bbt_chart timeline with run.py
+#post plotly chart via twitter and @mention originating account (me)
 #add color marker for cycle day 1
 #add color marker for temp spikes
 
