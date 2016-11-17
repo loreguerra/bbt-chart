a95bda3e61279f08f16aa1efb8f5d07b8dedafd8
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-11-17 07:45:33 -0800

    added request to data.py, installed oauthlib, updated requirements

--- data.py
+++ data.py
@@ -1,4 +1,15 @@
 import requests
-from requests.auth import OAuth1
+from requests_oauthlib import OAuth1
 
 from credentials import *
+
+url = u'https://api.twitter.com/1.1/account/settings.json'
+
+consumer_key = consumer_key
+consumer_secret = consumer_secret
+access_token = access_token
+access_token_secret = access_token_secret
+
+query_oauth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret, signature_type='query')
+
+r = request.get(url, auth=query_oauth)
