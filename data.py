45e061e1438309428bdf536c165ee6ed5d67f8f7
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-11-17 07:56:39 -0800

    added search url, created data as json

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ from requests_oauthlib import OAuth1
 
 from credentials import *
 
-url = 'https://api.twitter.com/1.1/account/settings.json'
+url = 'https://api.twitter.com/1.1/search/tweets.json?f=tweets&q=to%3Abbt_chart%20from%3Alorenaelise&src=typd'
 
 consumer_key = CONSUMER_KEY
 consumer_secret = CONSUMER_SECRET
@@ -12,4 +12,5 @@ access_token_secret = ACCESS_TOKEN_SECRET
 
 auth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
 
-requests.get(url, auth=auth)
+r = requests.get(url, auth=auth)
+data = r.json()
