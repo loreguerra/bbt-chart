8c91fbe010df7e6011b0188072b4f6707390ae49
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-11-17 07:49:03 -0800

    updated data.py

--- data.py
+++ data.py
@@ -3,13 +3,13 @@ from requests_oauthlib import OAuth1
 
 from credentials import *
 
-url = u'https://api.twitter.com/1.1/account/settings.json'
+url = 'https://api.twitter.com/1.1/account/settings.json'
 
-consumer_key = consumer_key
-consumer_secret = consumer_secret
-access_token = access_token
-access_token_secret = access_token_secret
+consumer_key = CONSUMER_KEY
+consumer_secret = CONSUMER_SECRET
+access_token = ACCESS_TOKEN
+access_token_secret = ACCESS_TOKEN_SECRET
 
-query_oauth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret, signature_type='query')
+auth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
 
-r = request.get(url, auth=query_oauth)
+requests.get(url, auth=auth)
