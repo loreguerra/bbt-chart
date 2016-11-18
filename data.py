4ef18114bed553c9dcf5588784053afc96a818ad
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-11-18 14:01:46 -0800

    added list comprehension to iterate and add temps :art:

--- data.py
+++ data.py
@@ -27,15 +27,15 @@ def get_temp(tweet):
     temp = float(temp) / 100
     return temp
 
+# def get_date(tweet):
+#
+
 temps = list(get_temp(tweet_data[i]['text']) for i in range(tweet_count))
 
+print tweet_data[0]['created_at']
 # may have to reverse lists to display correctly - 0 is newest tweet and should be oldest
 
 # date is 'created_at'
-
-# change to list comprehension
-# for i in range(tweet_count):
-#     print tweet_data[i]['text']
 
 # dump data for viewing
 # with open('data.json', 'w') as f:
