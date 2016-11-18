0dbbbc66adb76a8a0f9ef563ce65ce7ca7fa06df
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-11-18 14:03:22 -0800

    starting on get_date function

--- data.py
+++ data.py
@@ -27,12 +27,13 @@ def get_temp(tweet):
     temp = float(temp) / 100
     return temp
 
-# def get_date(tweet):
-#
+def get_date(tweet):
+    date =
 
 temps = list(get_temp(tweet_data[i]['text']) for i in range(tweet_count))
 
-print tweet_data[0]['created_at']
+get_date(tweet_data[0]['created_at'])
+
 # may have to reverse lists to display correctly - 0 is newest tweet and should be oldest
 
 # date is 'created_at'
