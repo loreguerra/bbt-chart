ae66ba0f27db4e0506f82ed139851f35c3c19b98
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-11-18 14:52:36 -0800

    imported dates and temps into run to create graph :fire:

--- run.py
+++ run.py
@@ -3,13 +3,15 @@ import plotly.graph_objs as go
 
 from credentials import set_credentials
 
+from data import temps, dates
+
 set_credentials()
 
 # days = [10, 11, 12, 13, 14]
 # temps = [97.36, 97.43, 97.37, 98.18, 96.9]
 
-dates = ['11-18-2016', '11-17-2016', '11-16-2016']
-temps = [97.36, 97.32, 96.69]
+dates = dates
+temps = temps
 
 #post plotly chart via twitter and @mention originating account (me)
 #add color marker for cycle day 1
