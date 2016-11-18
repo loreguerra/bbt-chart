4a971e65e49d4d42436b6fd276d5ebc6af417c92
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-11-18 14:53:27 -0800

    cleaned up comments :pencil:

--- data.py
+++ data.py
@@ -39,9 +39,6 @@ temps = list(get_temp(tweet_data[i]['text']) for i in range(tweet_count))
 
 dates = list(get_date(tweet_data[i]['created_at']) for i in range(tweet_count))
 
-# may have to reverse lists to display correctly - 0 is newest tweet and should be oldest
-
-# date is 'created_at'
 
 # dump data for viewing
 # with open('data.json', 'w') as f:
