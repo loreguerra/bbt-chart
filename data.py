7e20ce1d0faae260f8f01241fcd4901ee350cc50
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-12-08 13:10:47 -0800

    added functions to connect to db and add table

--- data.py
+++ data.py
@@ -6,10 +6,20 @@ database = db_info['database']
 user = db_info['user']
 password = db_info['password']
 
-conn = psycopg2.connect(database=database, user=user, password=password)
+def connect_to_db():
+    conn = psycopg2.connect(database=database, user=user, password=password)
+    return conn
 
 def create_temps_table():
+    conn = connect_to_db()
     cur = conn.cursor()
-    cur.execute(''' CREATE TABLE BBT_CHART; ''')
+    cur.execute("""CREATE TABLE BBT_CHART
+            (ID INT PRIMARY KEY  NOT NULL,
+            DATE DATE   NOT NULL,
+            TEMP REAL   NOT NULL);""")
     print 'Table created successfully'
     conn.commit()
+    conn.close()
+
+connect_to_db()
+create_temps_table()
