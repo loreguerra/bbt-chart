d6899e05a79c874f88b29d8c73126e4863307f7b
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-12-09 09:06:34 -0800

    cleaned up add_date file

--- add_data.py
+++ add_data.py
@@ -1,32 +1,9 @@
 import psycopg2
-
-from settings import db_info
-
 import sys
 
+from connect import connect_to_db
+
 # add argparse for options via command line
-
-# set database info variables
-database = db_info['database']
-user = db_info['user']
-password = db_info['password']
-
-# open connection function
-def connect_to_db():
-    conn = psycopg2.connect(database=database, user=user, password=password)
-    return conn
-
-# create table for data
-def create_temps_table():
-    conn = connect_to_db()
-    cur = conn.cursor()
-    cur.execute("""CREATE TABLE BBT_CHART
-            (ID INT PRIMARY KEY  NOT NULL,
-            DATE DATE   NOT NULL,
-            TEMP REAL   NOT NULL);""")
-    print 'Table created successfully'
-    conn.commit()
-    conn.close()
 
 # add new temperature and date
 def add_temp(date, temp):
