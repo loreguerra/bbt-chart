2d2d6b4c373559dd22da000b525fa77f894336ba
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-12-09 08:57:09 -0800

    added duplicate code from connect.py

---
+++ add_data.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+from settings import db_info
+
+import sys
+
+# add argparse for options via command line
+
+# set database info variables
+database = db_info['database']
+user = db_info['user']
+password = db_info['password']
+
+# open connection function
+def connect_to_db():
+    conn = psycopg2.connect(database=database, user=user, password=password)
+    return conn
+
+# create table for data
+def create_temps_table():
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE BBT_CHART
+            (ID INT PRIMARY KEY  NOT NULL,
+            DATE DATE   NOT NULL,
+            TEMP REAL   NOT NULL);""")
+    print 'Table created successfully'
+    conn.commit()
+    conn.close()
+
+# add new temperature and date
+def add_temp(date, temp):
+    print date, temp
+    # conn = connect_to_db()
