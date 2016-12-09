0b98b6a838ac72c93db042aaa330be283fddf1ba
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-12-09 09:21:35 -0800

    adding input into variables

--- add_data.py
+++ add_data.py
@@ -6,6 +6,9 @@ from connect import connect_to_db
 # add argparse for options via command line
 
 # add new temperature and date
+args = sys.argv
+date = args[1]
+temp = args[2]
+
 conn = connect_to_db()
 cur = conn.cursor()
-print sys.argv
