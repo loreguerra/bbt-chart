2ce8efa3bf227c9a769121a4d313963f0cfbde51
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-12-09 09:19:40 -0800

    print sys args

--- add_data.py
+++ add_data.py
@@ -6,6 +6,6 @@ from connect import connect_to_db
 # add argparse for options via command line
 
 # add new temperature and date
-def add_temp(date, temp):
-    print date, temp
-    # conn = connect_to_db()
+conn = connect_to_db()
+cur = conn.cursor()
+print sys.argv
