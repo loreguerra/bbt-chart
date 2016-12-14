c6577c51d2d4f4a7fd8c745fbb90d2bb32c9fdb1
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2016-12-13 16:00:52 -0800

    selecting dates and printing

--- run.py
+++ run.py
@@ -10,20 +10,23 @@ from connect import connect_to_db
 # set plotly credentials
 set_credentials()
 
-dates = dates
-temps = temps
+conn = connect_to_db()
+cur = conn.cursor()
+cur.execute("SELECT DATE from BBT_CHART")
+rows = cur.fetchall()
 
+for row in rows:
+    print row
 #post plotly chart via twitter and @mention originating account (me) - requests.post / convert to binary
 # cycle day 1 separate graph
 #add color marker for temp spikes
-#change usernames to variables
 
-trace = go.Scatter(
-    x = dates,
-    y= temps
-)
-
-data = [trace]
-
-plot_url = py.plot(data, filename='bbt')
-#save image, convert to binary, post to twitter, import auth from data
+# trace = go.Scatter(
+#     x = dates,
+#     y = temps
+# )
+#
+# data = [trace]
+#
+# plot_url = py.plot(data, filename='bbt')
+# #save image, convert to binary, post to twitter, import auth from data
