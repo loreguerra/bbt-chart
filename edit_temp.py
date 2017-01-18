a6f4114684c5b62960c47a6bfa87390da2e83e6b
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2017-01-18 06:54:56 -0800

    edited vars

--- edit_temp.py
+++ edit_temp.py
@@ -7,21 +7,21 @@ from connect import connect_to_db
 # filename counts as first arg
 args = sys.argv
 date_to_edit = args[1] # second arg
-new_date = args[2] #third arg
+new_temp = args[2] #third arg
 
 # adding items to data
-data = (new_date, date_to_edit)
+data = (new_temp, date_to_edit)
 
 # connect to database
 conn = connect_to_db()
 cur = conn.cursor()
 
 # SQL for inserting values into db
-SQL = "UPDATE BBT_CHART SET DATE = (%s) WHERE DATE = (%s);"
+SQL = "UPDATE BBT_CHART SET TEMP = (%s) WHERE DATE = (%s);"
 
 # execute SQL command plus data
 cur.execute(SQL, data)
 
-print "Date changed"
+print "Temperature changed"
 conn.commit()
 conn.close()
