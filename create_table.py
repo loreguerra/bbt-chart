bccdd482e838a29fe20e1f672e1aa9d288028177
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2017-01-27 15:56:51 -0800

    add extra column creation to create_table

--- create_table.py
+++ create_table.py
@@ -7,9 +7,9 @@ conn = connect_to_db()
 cur = conn.cursor()
 cur.execute("""CREATE TABLE BBT_CHART
         (DATE DATE   NOT NULL,
-        TEMP REAL   NOT NULL);""")
+        TEMP REAL   NOT NULL,
+        CYCLE_DAY VARCHAR(4),
+        LH_TEST CHAR(1));""")
 print 'BBT chart table created successfully'
 conn.commit()
 conn.close()
-
-# add in values for cycle_begin, pos_lh
