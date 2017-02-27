c79c07a0c275cd98063b39c6a0b5ed89d6d8a257
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2017-02-27 12:04:41 -0800

    refactor code, print variables

--- print_data.py
+++ print_data.py
@@ -13,11 +13,15 @@ SQL = "SELECT * FROM BBT_CHART ORDER BY DATE;"
 # execute SQL command plus data
 cur.execute(SQL)
 
+# add rows to variable
 rows = cur.fetchall()
 
+# loop over rows, for each row, create temporary variables to hold info
 for row in rows:
-    print row[0] + '\n'
-
+    # loop and list keys/values, convert to string and assign to vars
+    a, b, c, d = list(str(row[i]) for i in range(0,4))
+    # print variables with string formatting
+    print '%s \n%s \ncycle day: %s \nLH test: %s\n' % (a, b, c, d)
 
 conn.commit()
 conn.close()
