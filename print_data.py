c83455eec9937af99282764c13bc3d60e610ea34
Lorena Guerra Matteucci <loe.guerra@gmail.com>
2017-02-27 11:46:08 -0800

    add print_data file and code for fetching/printing rows

---
+++ print_data.py
@@ -0,0 +1,23 @@
+import psycopg2
+import sys
+
+from connect import connect_to_db
+
+# connect to database
+conn = connect_to_db()
+cur = conn.cursor()
+
+# SQL for inserting values into db
+SQL = "SELECT * FROM BBT_CHART ORDER BY DATE;"
+
+# execute SQL command plus data
+cur.execute(SQL)
+
+rows = cur.fetchall()
+
+for row in rows:
+    print str(row) + '\n'
+
+
+conn.commit()
+conn.close()
